02c67a95794d09b3bf23452feb5e06cd7295754f
f0nnys <448405043@qq.com>
2018-03-28 11:01:59 +0800

    day5 增加评论功能，过滤敏感词

--- src/main/java/com/roy/wenda/controller/QuestionController.java
+++ src/main/java/com/roy/wenda/controller/QuestionController.java
@@ -1,20 +1,22 @@
 package com.roy.wenda.controller;
 
 import com.roy.wenda.aspect.LogAspect;
-import com.roy.wenda.model.HostHolder;
-import com.roy.wenda.model.Question;
+import com.roy.wenda.model.*;
+import com.roy.wenda.service.CommentService;
 import com.roy.wenda.service.QuestionService;
+import com.roy.wenda.service.UserService;
 import com.roy.wenda.wendaUtil.WendaUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class QuestionController {
@@ -25,6 +27,12 @@ public class QuestionController {
 
     @Autowired
     HostHolder hostHolder;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    CommentService commentService;
 
     @RequestMapping(value = "/question/add",method = RequestMethod.POST)
     @ResponseBody
@@ -48,5 +56,22 @@ public class QuestionController {
             logger.error("增加题目失败"+e.getMessage());
         }
         return WendaUtil.getJSONString(1,"失败");
+    }
+
+    @RequestMapping(value = "/question/{qid}",method = RequestMethod.GET)
+    public String questionDetail(@PathVariable("qid") int qid,Model model){
+        Question question = questionService.selectById(qid);
+        model.addAttribute("question",question);
+
+        List<Comment> commentList = commentService.getCommentsByEntity(qid, EntityType.ENTITY_COMMENT);
+        List<ViewObject> comments = new ArrayList<>();
+        for(Comment comment:commentList){
+            ViewObject vo = new ViewObject();
+            vo.set("comment",comment);
+            vo.set("user",userService.getUser(comment.getUserId()));
+            comments.add(vo);
+        }
+        model.addAttribute("comments",comments);
+        return "detail";
     }
 }
